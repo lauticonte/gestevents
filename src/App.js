57253a1bbcb70e5324e4928e00fded880e691ff4
AlejoVeronese <alejoveronese2022@gmail.com>
2022-12-05 19:31:37 -0300

    Saco el Amplify Todos

--- src/App.js
+++ src/App.js
@@ -8,24 +8,11 @@ function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
 
       <div style={styles.container}>
         <Heading level={1}>Hello {user.username}</Heading>
         <Button onClick={signOut}>Sign out</Button>
-        <h2>Amplify Todos</h2>
       </div>
     </div>
   );
